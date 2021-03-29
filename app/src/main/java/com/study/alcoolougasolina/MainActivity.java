5aae46a66277876d3cce3d1184892d8135d940b4
anabibelforscher <anaolivercontact@gmail.com>
2021-03-29 12:49:50 -0300

    Cálculo comparativo

--- app/src/main/java/com/study/alcoolougasolina/MainActivity.java
+++ app/src/main/java/com/study/alcoolougasolina/MainActivity.java
@@ -3,12 +3,58 @@ package com.study.alcoolougasolina;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextInputEditText editPrecoAlcool, editPrecoGasolina;
+    private TextView textResultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editPrecoAlcool = findViewById(R.id.editPrecoAlcool);
+        editPrecoGasolina = findViewById(R.id.editPrecoGasolina);
+        textResultado = findViewById(R.id.textResultado);
     }
+
+     public void calcularPreco (View view) {
+        String precoAlcool = editPrecoAlcool.getText().toString();
+        String precoGasolina = editPrecoGasolina.getText().toString();
+
+        Boolean camposValidados = validarCampos(precoAlcool,precoGasolina);
+        if (camposValidados){
+            Double valorAlcool = Double.parseDouble(precoAlcool);
+            Double valorGasolina = Double.parseDouble(precoGasolina);
+            Double resultado = valorAlcool/valorGasolina;
+            if (resultado >= 0.7){
+                textResultado.setText("Melhor utilizar Gasolina");
+            } else {
+                textResultado.setText("Melhor usar Álcool");
+            }
+        }else {
+            Toast.makeText(getApplicationContext(), "Preencha os campos corretamente!",Toast.LENGTH_LONG).show();
+
+        }
+
+     }
+
+     public Boolean validarCampos (String pAlcool, String pGasolina){
+
+        Boolean camposValidados = true;
+
+        if ((pAlcool== null) || (pAlcool.equals(""))){
+            camposValidados=false;
+        } else if ((pGasolina== null) || (pGasolina.equals(""))){
+            camposValidados=false;
+
+         }
+        return camposValidados;
+     }
 }
